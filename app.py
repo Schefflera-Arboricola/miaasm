7d7a663c7cc4bf3a2a1a43ed5f832b5d5540847a
Aditi Juneja <91629733+Schefflera-Arboricola@users.noreply.github.com>
2026-04-26 21:27:57 +0530

    run on host 0.0.0.0 and port 8000

--- app.py
+++ app.py
@@ -115,4 +115,4 @@ def edit_desc():
 
 if __name__ == "__main__":
     debug = os.environ.get("FLASK_DEBUG", "0") == "1"
-    app.run(debug=debug)
+    app.run(host="0.0.0.0", port=8000, debug=debug)
